fcf7d8419697e8b9ba68c4ad318675718a776a3b
Akulov_V <Akulov_V@utkonos.ru>
2024-03-24 23:04:21 +0300

    MusicProject

---
+++ spring-app1/src/main/java/org/music/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package org.music;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ spring-app1/src/main/java/org/music/RockMusic.java
@@ -0,0 +1,12 @@
+package org;
+
+
+import org.springframework.stereotype.Component;
+
+@Component("musicBean")
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
